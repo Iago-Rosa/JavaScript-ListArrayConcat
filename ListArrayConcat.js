5f338deacc2c5dbe2208a2e1c7762288e72a3957
Iago-Rosa <iagorosa045@gmail.com>
2022-03-26 22:44:27 -0300

    arquivo

---
+++ ListArrayConcat.js
@@ -0,0 +1,15 @@
+
+//criando dois array e unindo-os em 1 array so
+var frutas = ["Pessego","Manga","Maça","limão"];
+var nomes = ["davi","iago","daniel","Nata"];
+
+//concatenando os dois arrays com o metodo concat
+var burro = frutas.concat(nomes)
+
+document.write(burro)
+console.log(burro)
+
+
+
+
+
